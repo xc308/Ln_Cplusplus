2e19ba0d242a9d32b6f48a91ab0b34234030e7cc
xc308 <xc308@exeter.ac.uk>
2023-03-25 22:40:30 +0000

    structure

---
+++ Ln_Cpp.R
@@ -0,0 +1,20 @@
+#========
+# Ln C++
+#========
+install.packages("Rcpp")
+library(Rcpp)
+
+
+##-------------------------
+# Structure of the program
+##-------------------------
+sourceCpp("Structure_of_program.cpp")
+
+
+
+
+
+
+
+
+
