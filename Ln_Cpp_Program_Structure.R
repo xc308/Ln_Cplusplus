b37503c8c4c9bfc136c02036235f7e100c93879f
xc308 <xc308@exeter.ac.uk>
2023-05-03 00:00:46 +0100

    arg by value and by ref

--- Ln_Cpp_Program_Structure.R
+++ Ln_Cpp_Program_Structure.R
@@ -235,17 +235,159 @@ sourceCpp("goto.cpp")
   # they are not valid C++ constant expressions.
 
 
+#-------------
+# Functions
+#-------------
+
+# a function is a group of statements that is given a name, 
+# and which can be called from some point of the program. 
+
+# most common syntax to define a function:
+  # type name ( parameter1, parameter2, ...) { statements }
+    
+    # type :  the type of the value returned by the function.
+    # name : the identifier by which the function can be called.
+    # parameters (as many as needed):
+        # Each parameter consists of a type followed by an identifier,
+        # parameter being separated from the next by a comma
+        # each parameter looks very much like a regular variable declaration (for example: int x), 
+        # acts within the function as a regular variable which is local to the function.
+
+    # The purpose of parameters is to allow passing arguments to the function 
+        # from the location where it is called from.
+    
+    #  statements: the function's body.   
+           #  a block of statements surrounded by braces { } that specify 
+            # what the function actually does  
+
+    
+library(Rcpp)
+sourceCpp("function.cpp")
+
+# a C++ program always starts by calling main
+# main is the only function called automatically, 
+  # and the code in any other function is only executed if its function is called from main
 
 
 
+#----------------------------------------
+# Functions with no type. The use of void
+#----------------------------------------
+
+# the function does not need to return a value
+  # the type to be used is void
+      # a special type to represent the absence of value.
+
+# a function that simply prints a message may not need to return any value:
+
+
+library(Rcpp)
+sourceCpp("void.cpp")
+
+# void can also be used in the function's parameter list 
+  # to explicitly specify that the function 
+    # takes no actual parameters when called.
+
+  # void printmessage (void) {
+      # cout << "I'm a function!";
+      # }
+
+# In C++, an empty parameter list can be used instead of void 
+    # with same meaning,
+  
+# an empty pair of parentheses shall always be appended to the function name
+
+# the parentheses are what differentiate functions from other kinds of declarations or statements
+
+# printmessage ();
+# vs printmessage;
 
 
 
+#---------------------------
+# The return value of main
+#---------------------------
+
+# the return type of main is int, 
+# but most examples in this and earlier chapters did not 
+    # actually return any value from main.
+
+
+#  If the execution of main ends normally without 
+    # encountering a return statement 
+      # the compiler assumes the function ends with an implicit 
+        # return statement:1 return 0;
+
+#  this only applies to function main 
+
+# All other functions with a return type shall end with 
+  # a proper return statement that includes a return value,
+
+# When main returns zero (either implicitly or explicitly), 
+  # it is interpreted by the environment as that 
+    # the program ended successfully.
 
 
 
+#--------------------------------------------
+# Arguments passed by value and by reference
+#--------------------------------------------
+
+# arguments have always been passed by value
+  # when calling a function, what is passed to the function 
+    # are the values of these arguments on the moment of the call
+  # are copied into the variables represented by the function parameters
+
+    # int x=5, y=3, z;
+    # z = addition ( x, y );
+
+# These values (5 and 3) are used to initialize the variables 
+  # set as parameters in the function's definition
+
+#  but any modification of these variables within the function 
+  # has no effect on the values of the variables x and y outside it, 
+  # because x and y were themselves not passed to the function on the call, 
+    # but only copies of their values at that moment.
 
 
+#  may be useful to access an external variable from within a function
+  # arguments can be passed by reference, instead of by value. 
+
+  # the function duplicate in this code duplicates 
+    # the value of its three arguments, 
+      # causing the variables used as arguments 
+        # to actually be modified by the call:
+
+
+# To gain access to its arguments, the function declares its parameters as references.
+  # In C++, references are indicated with an ampersand (&) following the parameter type,
+
+# When a variable is passed by reference, 
+  # what is passed is no longer a copy,
+    # but the variable itself, 
+      # the variable identified by the function parameter, 
+        # becomes associated with the argument passed to the function, 
+          # and any modification on their corresponding local variables
+            # within the function are reflected in the variables 
+              # passed as arguments in the call.
+
+
+# In fact, a, b, and c become aliases of the arguments 
+  # passed on the function call (x, y, and z) 
+      # and any change on a within the function 
+        # is actually modifying variable x outside the function. 
+          # Any change on b modifies y, and any change on c modifies z. 
+
+
+library(Rcpp)
+sourceCpp("assign_par_by_reference.cpp")
+
+# Note that if modify the function as not as by reference 
+# void duplicate(int a, int b, int c)
+# NOT void duplicate(int& a, int& b, int& c)
+  # then if set x = 1, y = 2, z = 7
+  # duplicate(x, y, z) would just duplicate x, y, z as 2x, 2y, 2z
+      # rather than duplicate the values
 
 
 
