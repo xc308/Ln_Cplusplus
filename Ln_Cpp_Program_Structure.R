1472ae8472d2ac4ac747a2932e849cea4b60dc44
xc308 <xc308@exeter.ac.uk>
2023-05-09 23:25:02 +0100

    namespace

--- Ln_Cpp_Program_Structure.R
+++ Ln_Cpp_Program_Structure.R
@@ -794,3 +794,57 @@ sourceCpp("non-type_temp_arg.cpp")
 library(Rcpp)
 sourceCpp("block_scope.cpp")
 
+
+#-----------
+# Namespaces
+#-----------
+
+# Only one entity can exist with a particular name in a particular scope.
+# But non-local names bring more possibilities for 
+  # name collision, especially considering that libraries may declare many functions, types, and variables, neither of them local in nature, and some of them very generic.
+
+# Namespaces allow us to group named entities 
+  # that otherwise would have global scope 
+    # into narrower scopes, giving them namespace scope. 
+
+# This allows organizing the elements of programs 
+  # into different logical scopes referred to by names.
+
+# The syntax to declare a namespaces is:
+  # namespace identifier {
+      # named_entities
+  # }
+
+    # identifier is any valid identifier
+    # named_entities is the 
+      # set of variables, types and functions 
+        # that are included within the namespace.
+
+
+# namespace myNamespace {
+  #int a, b;
+#}
+
+# the variables a and b are normal variables declared within a namespace called myNamespace.
+
+# These variables can be accessed from 
+  # within their namespace normally, 
+    # with their identifier (either a or b), 
+      # but if accessed from outside the myNamespace namespace 
+        # they have to be properly qualified with the scope operator ::
+
+# to access the previous variables from outside myNamespace they should be qualified like:
+      # myNamespace::a
+      # myNamespace::b
+
+# Namespaces are particularly useful to avoid name collisions.
+
+library(Rcpp)
+sourceCpp("namespace.cpp")
+
+
+
+
+
+
+
