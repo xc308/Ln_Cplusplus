c200534c14104211ebd248b42350874ab6eddb36
xc308 <xc308@exeter.ac.uk>
2023-06-17 22:51:38 +0100

    pointers to functions

--- Ln_Cpp_Compund_Data_Types.R
+++ Ln_Cpp_Compund_Data_Types.R
@@ -759,6 +759,17 @@ sourceCpp("pointers_as_arg_fun.cpp")
                   # before it is dereference. 
 
 
+#---------
+# Pointers to functions
+#---------
+
+# The typical use of this is for passing a function as an argument to another function.
+# Pointers to functions are declared with the same syntax as 
+  # a regular function declaration, 
+    # except that the name of the function is enclosed between 
+      # parentheses () and an asterisk (*) is inserted before the name:
+
+
 
 
 
